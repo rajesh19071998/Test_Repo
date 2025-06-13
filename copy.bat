4e747d995e1928867dd4460a4673984fc725d3d3
Rajesh V <108512@grundfos.com>
2025-06-13 12:19:51 +0530

    copy bat created

---
+++ copy.bat
@@ -0,0 +1,45 @@
+@echo off
+
+REM ******************************************************
+REM This script is for Copy Bin action 
+REM This Batch file is used for the GEN6 Project
+REM Only _bin files will be copied to SW_OUT folder 
+REM commneted _gen and _out folder copy since we are not using 
+REM AUTHOR: Config Team
+REM ******************************************************
+
+git submodule init
+git submodule update
+
+
+set ota_version=V00.000.001
+REM set version=V+
+
+set appl_version=V00.000.001
+echo %ota_version%
+
+setlocal enabledelayedexpansion
+
+
+cd /d %~dp0
+
+set appl_path= Src\%appl_version%
+set mit_app_path= build\deps\ota_mit_app\Src\%ota_version%\OTA_MIT_App\MIT_APP.h
+
+
+
+IF NOT EXIST %appl_path% (
+echo [Warning]: %appl_path% File not found
+Exit
+)
+
+
+IF NOT EXIST %mit_app_path% (
+echo [Warning]: %mit_app_path% File not found
+Exit
+)
+
+copy %mit_app_path% %appl_path%
+
+
+echo created
